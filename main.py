4af06abd1697e169558b2931edf370ae0b89f0f2
Silvia Hernandez <silviaherf@gmail.com>
2020-09-21 14:02:37 +0200

    funcion get_repos creada, pendiente bucle paginas

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import src.api as api 
 
-
-
-
-
-
-
-
-
-
 def main():
+    response=api.get_pr()
+
+    print('Loading page 1')
+
+    """
+    reviews=api.api_to_df(response)
+    i=1
+    while response['has_more']==True:
+        try:
+            print(f'Loading page {i+1}')
+            reviews=reviews.append(api.api_to_df(api.get_pr(args,i=i))).reset_index()
+            i+=1
+        except ValueError:
+            break
+
+
+"""
+if __name__=="__main__":
+    main()
--- src/api.py
+++ src/api.py
@@ -2,31 +2,36 @@ import os
 from dotenv import load_dotenv
 import requests
 
-def get_repos(endpoint='/repos/ironhack-datalabs/datamad0820/pulls',api_key=os.getenv('GH_APIKEY'),query_params={}):
+def get_pr(i=0,api_key=os.getenv('GH_APIKEY'),i=0):
+    
     """
     This function gets information out of an API for the year previously entered as a terminal argument.
     """
     baseUrl="https://api.github.com"
+    endpoint=f'/repos/ironhack-datalabs/datamad0820/pulls?state=closed&page={i}'
     url = f"{baseUrl}{endpoint}"
 
 
     headers = {
         "Authorization": f"Bearer {api_key}"
     }
-    # make the request and get the response using HTTP GET verb 
-    res = requests.get(url, params=query_params, headers=headers)
+
+    res = requests.get(url, params=query_params)
+    #res = requests.get(url, params=query_params, headers=headers)
     print(f"Request data to {res.url} status_code:{res.status_code}")
     
-    data = res.json()
+ 
 
 
     if res.status_code != 200:
-        data=res.json()
+        
         raise ValueError(f'Invalid Github API call: {data["message"]}\nSee more in {data["documentation_url"]}')
         
     else:
         print(f"Requested data to {baseUrl}; status_code:{res.status_code}")
-        data=res.json()
-        return data
+        
+        return res
+
+
 
 
