64bc0b84b09d7250d12b2a29599e73d8f3474ea0
Silvia Hernandez <silviaherf@gmail.com>
2020-09-22 11:46:08 +0200

    api_gen.py con endpoints proyecto y endopint prueba hecho

--- src/api_gen.py
+++ src/api_gen.py
@@ -2,11 +2,13 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 import requests
-#from flask import request, Response
+from flask import Flask
+from flask import request, Response
 #from src.helpers.json_response import asJsonResponse
 import re
-from src.database import db
+from database import db
 
+app = Flask("ranking")
 
 """
 
@@ -37,7 +39,195 @@ def searchStudent(studentname):
     return {
         "status": "OK",
         "searchQuery": studentname,
-        "company": foundStudent
+        "student": foundStudent
     }
 
 """
+
+
+@app.route("/student/create/prueba")
+#@asJsonResponse
+def searchStudent():
+    
+    return {
+        "status": "OK",
+        "message": 'esto es una prueba',
+
+    }
+
+"""
+@app.route("/student/all")
+#@asJsonResponse
+def allStudents():
+    
+    if not studentname:
+        # Set status code to 400 BAD REQUEST
+        return {
+            "status": "error",
+            "message": "Any student in query , please specify one"
+        }, 400
+
+    # Search a company in mongodb database
+    projection = {"name": 1, "category_code": 1,"description":1}
+    searchRE = re.compile(f"{companyNameQuery}", re.IGNORECASE)
+    foundStudent = db["crunchbase"].find_one(
+        {"name": searchRE}, projection)
+
+    if not foundStudent:
+        # Set status code to 404 NOT FOUND
+        return {
+            "status": "not found",
+            "message": f"No student found with name {foundStudent} in database"
+        }, 404
+
+    return {
+        "status": "OK",
+        "searchQuery": studentname,
+        "student": foundStudent
+    }
+
+"""
+
+
+
+
+
+
+"""
+
+@app.route("/lab/create")
+#@asJsonResponse
+def searchLab(lab-prefix):
+    
+    if not lab-prefix:
+        # Set status code to 400 BAD REQUEST
+        return {
+            "status": "error",
+            "message": "Any student in query , please specify one"
+        }, 400
+
+    # Search a company in mongodb database
+    projection = {"name": 1, "category_code": 1,"description":1}
+    searchRE = re.compile(f"{companyNameQuery}", re.IGNORECASE)
+    foundStudent = db["crunchbase"].find_one(
+        {"name": searchRE}, projection)
+
+    if not foundStudent:
+        # Set status code to 404 NOT FOUND
+        return {
+            "status": "not found",
+            "message": f"No student found with name {foundStudent} in database"
+        }, 404
+
+    return {
+        "status": "OK",
+        "searchQuery": lab-prefix,
+        "student": foundStudent
+    }
+
+"""
+
+
+
+"""
+
+@app.route("/lab/<lab_id>/search")
+#@asJsonResponse
+def searchLab(lab-prefix):
+    
+    if not lab-prefix:
+        # Set status code to 400 BAD REQUEST
+        return {
+            "status": "error",
+            "message": "Any student in query , please specify one"
+        }, 400
+
+    # Search a company in mongodb database
+    projection = {"name": 1, "category_code": 1,"description":1}
+    searchRE = re.compile(f"{companyNameQuery}", re.IGNORECASE)
+    foundStudent = db["crunchbase"].find_one(
+        {"name": searchRE}, projection)
+
+    if not foundStudent:
+        # Set status code to 404 NOT FOUND
+        return {
+            "status": "not found",
+            "message": f"No student found with name {foundStudent} in database"
+        }, 404
+
+    return {
+        "status": "OK",
+        "searchQuery": lab-prefix,
+        "student": foundStudent
+    }
+
+"""
+"""
+
+@app.route("/lab/memeranking")
+#@asJsonResponse
+def searchLab(lab-prefix):
+    
+    if not lab-prefix:
+        # Set status code to 400 BAD REQUEST
+        return {
+            "status": "error",
+            "message": "Any student in query , please specify one"
+        }, 400
+
+    # Search a company in mongodb database
+    projection = {"name": 1, "category_code": 1,"description":1}
+    searchRE = re.compile(f"{companyNameQuery}", re.IGNORECASE)
+    foundStudent = db["crunchbase"].find_one(
+        {"name": searchRE}, projection)
+
+    if not foundStudent:
+        # Set status code to 404 NOT FOUND
+        return {
+            "status": "not found",
+            "message": f"No student found with name {foundStudent} in database"
+        }, 404
+
+    return {
+        "status": "OK",
+        "searchQuery": lab-prefix,
+        "student": foundStudent
+    }
+
+"""
+
+
+"""
+
+@app.route("/lab/<lab_id>/meme")
+#@asJsonResponse
+def searchLab(lab-prefix):
+    
+    if not lab-prefix:
+        # Set status code to 400 BAD REQUEST
+        return {
+            "status": "error",
+            "message": "Any student in query , please specify one"
+        }, 400
+
+    # Search a company in mongodb database
+    projection = {"name": 1, "category_code": 1,"description":1}
+    searchRE = re.compile(f"{companyNameQuery}", re.IGNORECASE)
+    foundStudent = db["crunchbase"].find_one(
+        {"name": searchRE}, projection)
+
+    if not foundStudent:
+        # Set status code to 404 NOT FOUND
+        return {
+            "status": "not found",
+            "message": f"No student found with name {foundStudent} in database"
+        }, 404
+
+    return {
+        "status": "OK",
+        "searchQuery": lab-prefix,
+        "student": foundStudent
+    }
+
+"""
+
--- src/server.py
+++ src/server.py
@@ -1,5 +1,7 @@
+import os
 from dotenv import load_dotenv
 load_dotenv()
+from api_gen import app
 
 PORT = os.getenv("PORT")
 app.run("0.0.0.0", PORT, debug=True)
