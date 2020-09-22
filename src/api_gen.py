17ea225ed2c13f3bd6f35da0f95cdebe2bce040a
Silvia Hernandez <silviaherf@gmail.com>
2020-09-22 20:04:39 +0200

    endpoint students/all hecho

--- src/api_gen.py
+++ src/api_gen.py
@@ -4,24 +4,22 @@ load_dotenv()
 import requests
 from flask import Flask
 from flask import request, Response
-#from src.helpers.json_response import asJsonResponse
 import re
 from src.database import db
 import src.data_exportation as export
+import json
 
 app = Flask("ranking")
 
 
 @app.route("/student/create/<studentname>")
 def createStudent(studentname):
-   if not studentname:
-        # Set status code to 400 BAD REQUEST
+    if not studentname:
         return {
             "status": "error",
             "message": "Any student in query , please specify one"
         }, 400
-
-    if db.github.find(f'"name":{studentname}'):
+    if db.students.find_one({"name": studentname}):
         return 'This student is already on MongoDB'
     else:
         stu=export.student_toMongo(student)
@@ -29,52 +27,14 @@ def createStudent(studentname):
         
 
 
-"""
-@app.route("/student/create/prueba")
-#@asJsonResponse
-def searchStudent():
-    
-    return {
-        "status": "OK",
-        "message": 'esto es una prueba',
-
-    }
-
-"""
-
-
-"""
 @app.route("/student/all")
-#@asJsonResponse
 def allStudents():
-    
-    if not studentname:
-        # Set status code to 400 BAD REQUEST
-        return {
-            "status": "error",
-            "message": "Any student in query , please specify one"
-        }, 400
 
-    # Search a company in mongodb database
-    projection = {"name": 1, "category_code": 1,"description":1}
-    searchRE = re.compile(f"{companyNameQuery}", re.IGNORECASE)
-    foundStudent = db["crunchbase"].find_one(
-        {"name": searchRE}, projection)
+    cursor=db.students.find({"_id":{'$exists':True}},{'_id':0})
 
-    if not foundStudent:
-        # Set status code to 404 NOT FOUND
-        return {
-            "status": "not found",
-            "message": f"No student found with name {foundStudent} in database"
-        }, 404
+    return json.dumps(list(cursor))
 
-    return {
-        "status": "OK",
-        "searchQuery": studentname,
-        "student": foundStudent
-    }
 
-"""
 
 
 
